8027fd1e2739e1e79a6b7473b6e0365f6cf997f7
Bieelogrom <gabrielamorim15436@gmail.com>
2023-10-08 14:26:05 -0300

    1.0

--- Model/usuario.php
+++ Model/usuario.php
@@ -5,88 +5,213 @@
 class Usuario{
     protected $idUsuario;
     protected $nomeUsuario;
-    protected $emailUsuario;
-    protected $senhaUsuario;
-    protected $telefoneUsuario;
-    protected $dataNascimentoUsuario;
     protected $apelidoUsuario;
-    protected $fotoDePerfil;
-    protected $tipoDePerfil;
+    protected $emailUsuario;
+    protected $telefoneUsuario;
+    protected $senhaUsuario;
+    protected $nascUsuario;
+    protected $fotoUsuario;
+    protected $capaUsuario;
+    protected $statusConta;
+    protected $nivelConta;
+    protected $tipoConta;
 
-    public function getTipoDePerfil(){
-        return $this->tipoDePerfil;
-    }
 
-    public function setTipoDePerfil($tipoDePerfil){
-        $this->tipoDePerfil = $tipoDePerfil;
-    }
 
-    public function getFotoDePerfil(){
-        return $this->fotoDePerfil;
-    }
-
-    public function setfotoDePerfil($fotoDePerfil){
-        $this->fotoDePerfil = $fotoDePerfil;
-    }
-
-    public function getIdUsuario(){
-        return $this->idUsuario;
-    }
-
-    public function setIdUsuario($idUsuario){
-        $this->idUsuario = $idUsuario;
-    }
-
-    public function getNomeUsuario(){
-        return $this->nomeUsuario;
-    }
-    
-    public function setNomeUsuario($nomeUsuario){
-        $this->nomeUsuario = $nomeUsuario;
-    }
-
-    public function getEmailUsuario(){
-        return $this->emailUsuario;
-    }
-
-    public function setEmailUsuario($emailUsuario){
-        $this->emailUsuario = $emailUsuario;
-    }
-
-    public function getSenhaUsuario(){
-        return $this->senhaUsuario;
-    }
-
-    public function setSenhaUsuario($senhaUsuario){
-        $this->senhaUsuario = $senhaUsuario;
-    }
-
-    public function getTelefoneUsuario(){
-        return $this->telefoneUsuario;
-    }
-
-    public function setTelefoneUsuario($telefoneUsuario){
-        $this->telefoneUsuario = $telefoneUsuario;
-    }
-
-    
-    public function getDataNascimentoUsuario(){
-        return $this->dataNascimentoUsuario;
-    }
-
-    public function setDataNascimentoUsuario($dataNascimentoUsuario){
-        $this->dataNascimentoUsuario = $dataNascimentoUsuario;
-    }
-
-    public function getApelidoUsuario(){
-        return $this->apelidoUsuario;
-    }
-
-    public function setApelidoUsuario($apelidoUsuario){
-        $this->apelidoUsuario = $apelidoUsuario;
-    }
 
     
 
+
+	/**
+	 * @return mixed
+	 */
+	public function getIdUsuario() {
+		return $this->idUsuario;
+	}
+	
+	/**
+	 * @param mixed $idUsuario 
+	 * @return self
+	 */
+	public function setIdUsuario($idUsuario): self {
+		$this->idUsuario = $idUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getNomeUsuario() {
+		return $this->nomeUsuario;
+	}
+	
+	/**
+	 * @param mixed $nomeUsuario 
+	 * @return self
+	 */
+	public function setNomeUsuario($nomeUsuario): self {
+		$this->nomeUsuario = $nomeUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getApelidoUsuario() {
+		return $this->apelidoUsuario;
+	}
+	
+	/**
+	 * @param mixed $apelidoUsuario 
+	 * @return self
+	 */
+	public function setApelidoUsuario($apelidoUsuario): self {
+		$this->apelidoUsuario = $apelidoUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getEmailUsuario() {
+		return $this->emailUsuario;
+	}
+	
+	/**
+	 * @param mixed $emailUsuario 
+	 * @return self
+	 */
+	public function setEmailUsuario($emailUsuario): self {
+		$this->emailUsuario = $emailUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getTelefoneUsuario() {
+		return $this->telefoneUsuario;
+	}
+	
+	/**
+	 * @param mixed $telefoneUsuario 
+	 * @return self
+	 */
+	public function setTelefoneUsuario($telefoneUsuario): self {
+		$this->telefoneUsuario = $telefoneUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getSenhaUsuario() {
+		return $this->senhaUsuario;
+	}
+	
+	/**
+	 * @param mixed $senhaUsuario 
+	 * @return self
+	 */
+	public function setSenhaUsuario($senhaUsuario): self {
+		$this->senhaUsuario = $senhaUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getNascUsuario() {
+		return $this->nascUsuario;
+	}
+	
+	/**
+	 * @param mixed $nascUsuario 
+	 * @return self
+	 */
+	public function setNascUsuario($nascUsuario): self {
+		$this->nascUsuario = $nascUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getFotoUsuario() {
+		return $this->fotoUsuario;
+	}
+	
+	/**
+	 * @param mixed $fotoUsuario 
+	 * @return self
+	 */
+	public function setFotoUsuario($fotoUsuario): self {
+		$this->fotoUsuario = $fotoUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getCapaUsuario() {
+		return $this->capaUsuario;
+	}
+	
+	/**
+	 * @param mixed $capaUsuario 
+	 * @return self
+	 */
+	public function setCapaUsuario($capaUsuario): self {
+		$this->capaUsuario = $capaUsuario;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getStatusConta() {
+		return $this->statusConta;
+	}
+	
+	/**
+	 * @param mixed $statusConta 
+	 * @return self
+	 */
+	public function setStatusConta($statusConta): self {
+		$this->statusConta = $statusConta;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getNivelConta() {
+		return $this->nivelConta;
+	}
+	
+	/**
+	 * @param mixed $nivelConta 
+	 * @return self
+	 */
+	public function setNivelConta($nivelConta): self {
+		$this->nivelConta = $nivelConta;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getTipoConta() {
+		return $this->tipoConta;
+	}
+	
+	/**
+	 * @param mixed $tipoConta 
+	 * @return self
+	 */
+	public function setTipoConta($tipoConta): self {
+		$this->tipoConta = $tipoConta;
+		return $this;
+	}
 }
 ?>
--- dao/logoff.php
+++ dao/logoff.php
@@ -4,5 +4,4 @@
         session_destroy();
         header('Location: ../index.php');
         exit();
-
 ?>
